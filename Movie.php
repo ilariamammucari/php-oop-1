8b5e60f629c78ed284931c9d6f5362cdc946a628
ilariamammucari <>
2021-03-12 13:49:53 +0100

    costruttore e stampo valore obbligato

--- Movie.php
+++ Movie.php
@@ -5,4 +5,11 @@ class Movie {
     public $trama;
     public $cast;
     public $anno;
+
+    public function __construct($_titolo) {
+        $this->titolo = $_titolo;
+    }
 }
+
+$film = new Movie('Harry Potter');
+echo $film->titolo;
